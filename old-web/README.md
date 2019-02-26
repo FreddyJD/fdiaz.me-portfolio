8c2e3cd0d386a5fe06971f143f6670cfe9b71be7
Freddy <freddy.jd2@gmail.com>
2019-02-25 22:12:11 -0500

    Updated portfolio

---
+++ old-web/README.md
@@ -0,0 +1,2 @@
+# freddyjd.github.io
+ Simple portfolio
---
+++ old-web/includes/custom.js
@@ -0,0 +1,90 @@
+// Welcome to my portfolio. 
+// Inspecting element 🕵️‍ ? What a detective! 
+
+// If you are trying to find out how I do things
+// you can just email me @ ✅ git.fdiaz@gmail.com 
+
+// 🔥 hit me up with your idea/project. I try 
+// to say yes to everything....
+
+
+var $ = function (selector) {
+    return document.querySelectorAll(selector);
+  };
+  
+  var tabs = [
+    '.tabbed-section__selector-tab-1',
+    '.tabbed-section__selector-tab-2',
+    '.tabbed-section__selector-tab-3',
+  ];
+  
+  var toggleTab = function(element) {
+    var parent = element.parentNode;
+    
+    $(element)[0].addEventListener('click', function(){
+      this.parentNode.childNodes[1].classList.remove('active');
+      this.parentNode.childNodes[3].classList.remove('active');
+      this.parentNode.childNodes[5].classList.remove('active');
+      this.parentNode.childNodes[7].classList.remove('active');
+
+  
+      this.classList.add('active');
+      
+      if(this.classList.contains('tabbed-section__selector-tab-1')) {
+        $('.tabbed-section-1')[0].classList.remove('hidden');
+        $('.tabbed-section-1')[0].classList.add('visible');
+    
+        $('.tabbed-section-2')[0].classList.remove('visible');
+        $('.tabbed-section-2')[0].classList.add('hidden');
+
+         $('.tabbed-section-3')[0].classList.remove('visible');
+        $('.tabbed-section-3')[0].classList.add('hidden');
+
+      }
+  
+      if(this.classList.contains('tabbed-section__selector-tab-2')) {
+        $('.tabbed-section-2')[0].classList.remove('hidden');
+        $('.tabbed-section-2')[0].classList.add('visible');
+        $('.tabbed-section-1')[0].classList.remove('visible');
+        $('.tabbed-section-1')[0].classList.add('hidden');
+        $('.tabbed-section-3')[0].classList.remove('visible');
+        $('.tabbed-section-3')[0].classList.add('hidden');
+      }
+      
+      if(this.classList.contains('tabbed-section__selector-tab-3')) {
+        $('.tabbed-section-3')[0].classList.remove('hidden');
+        $('.tabbed-section-3')[0].classList.add('visible');
+
+        $('.tabbed-section-1')[0].classList.remove('visible');
+        $('.tabbed-section-1')[0].classList.add('hidden');
+        $('.tabbed-section-2')[0].classList.remove('visible');
+        $('.tabbed-section-2')[0].classList.add('hidden');
+
+      }
+
+      if(this.classList.contains('tabbed-section__selector-tab-4')) {
+
+        $('.tabbed-section-1')[0].classList.remove('visible');
+        $('.tabbed-section-1')[0].classList.add('hidden');
+        $('.tabbed-section-2')[0].classList.remove('visible');
+        $('.tabbed-section-2')[0].classList.add('hidden');
+
+        $('.tabbed-section-3')[0].classList.remove('visible');
+        $('.tabbed-section-3')[0].classList.add('hidden');
+      }
+
+    });
+  };
+  
+  for (var i = tabs.length - 1; i >= 0; i--) {
+    toggleTab(tabs[i])
+  };
+
+  var typed = new Typed("#typed", {
+    strings: ["^200Project ^3000", "^1000 Freelance Work^3000", "^1000 Job ^3000"],
+    typeSpeed: 100,
+    smartBackspace: true,
+    loop: true
+  });
+
+  
